90b46f367c36a65e549259de4da42abdaa71fef3
Umut Topalak <umuttopalak@hotmail.com>
2024-06-07 22:03:55 +0300

    firebase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:finance_tracking/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
